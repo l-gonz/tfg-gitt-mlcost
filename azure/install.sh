d773856650751557a89b6f64f377dea865c58c1b
azure-machine <azure.machine@email.com>
2024-06-01 11:09:42 +0000

    Measure electricity in Azure machine
    model name	: Intel(R) Xeon(R) Platinum 8272CL CPU @ 2.60GHz
    MemTotal:        8078108 kB

--- azure/install.sh
+++ azure/install.sh
@@ -12,7 +12,7 @@ sudo apt install -y python3.12
 sudo apt install -y python3.12-venv
 
 ghtoken=$(az keyvault secret show -n Github-Token --vault-name kv-mlcost --query "value" -o tsv)
-cd ~/tfg-gitt-mlcost-azure
+cd ~/tfg-gitt-mlcost
 git remote set-url origin https://azure-machine:${ghtoken}@github.com/l-gonz/tfg-gitt-mlcost.git
 git config --local user.email "azure.machine@email.com"
 git config --local user.name "azure-machine"
