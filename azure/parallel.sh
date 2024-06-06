5469f218a51a218d0fa5a2fe8be8202c95fbb5a5
Laura Gonzalez <l.gonzalezfernan@gmail.com>
2024-06-06 08:04:49 +0200

    Update azure script

---
+++ azure/parallel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+timestamp=$(date +%F_%T)
+python -m mlcost measure --log -cv 5 --openml -d eeg-eye-state
+python -m mlcost measure --log -cv 5 --openml -d eeg-eye-state --parallel
+python -m mlcost measure --log -cv 5 --openml -d electricity
+python -m mlcost measure --log -cv 5 --openml -d electricity --parallel
+
+mv output.csv out/output_parallel_${timestamp}.csv
+
+git add *
+git commit -m "Measure parallel run in Azure machine" -m "$(cat /proc/cpuinfo  | grep 'name'| uniq)
+$(cat /proc/meminfo | grep 'MemTotal')"
+git push origin
